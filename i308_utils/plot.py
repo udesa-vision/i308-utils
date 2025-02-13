216453880d04e658a48560aad7eab74e56426a6e
esteban <estebanuri@gmail.com>
2025-02-13 11:54:45 -0300

    plot preparation was unified for imshow and show_images. if image data type is uint8 sets 0 and 255 as min and max values. Else lets mpl to normalize it

--- i308_utils/plot.py
+++ i308_utils/plot.py
@@ -1,39 +1,41 @@
-import matplotlib.pyplot as plt
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def prepare_plot_args(image):
+    """prepara los argumentos para imshow según el tipo de imagen"""
+    if len(image.shape) == 2:  # imagen en escala de grises
+        cmap = 'gray'
+        vmin, vmax = (0, 255) if image.dtype == np.uint8 else (None, None)
+        return image, {'cmap': cmap, 'vmin': vmin, 'vmax': vmax}
+    else:  # imagen en color (asume BGR)
+        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), {}
+
+
+def display_image(ax, image, title=None):
+    """muestra una imagen en un eje matplotlib"""
+    img_data, plot_args = prepare_plot_args(image)
+    ax.imshow(img_data, **plot_args)
+    ax.axis('off')
+    if title:
+        ax.set_title(title, fontsize=10)
 
 
 def imshow(image, title=None):
-    """
-
-        Grafica una imagen usando matplotlib
-
-    """
-    if len(image.shape) == 2:  # Imagen en escala de grises
-        plt.imshow(image, cmap='gray')
-    else:  # Imagen en color (asumimos BGR)
-        im = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        plt.imshow(im)
-
-    if title is not None:
-        plt.title(title, fontsize=10)
-    plt.axis('off')
+    """grafica una imagen usando matplotlib"""
+    fig, ax = plt.subplots()
+    display_image(ax, image, title)
     plt.show()
 
 
-def show_images(
-        images,
-        titles=None,
-        grid=None,
-        figsize=None,
-        title=None,
-        subtitle=None,
-        show=True
-):
+def show_images(images, titles=None, grid=None, figsize=None, title=None, subtitle=None, show=True):
+    """muestra múltiples imágenes en una cuadrícula"""
     if grid is None:
         grid = (1, len(images))
 
     if titles is None:
-        titles = [None for _ in images]
+        titles = [None] * len(images)
 
     n_rows, n_cols = grid
     if figsize is None:
@@ -41,27 +43,18 @@ def show_images(
         h = int(1.2 * n_rows * w / n_cols)
         figsize = (w, h)
 
-    # print(figsize)
     fig, axes = plt.subplots(n_rows, n_cols, figsize=figsize)
 
-    # fig.patch.set_facecolor('lightblue')
+    if isinstance(axes, np.ndarray):
+        axes = axes.flat
+    else:
+        axes = [axes]
 
-    for i, ax in enumerate(axes.flat):
-
-        ax.axis('off')
-        # ax.set_facecolor('green')
-        if i >= len(images):
-            continue
-
-        image = images[i]
-        if len(image.shape) == 2:  # imagen en escala de grises
-            ax.imshow(images[i], cmap='gray', vmin=0, vmax=255)
-        else:  # imagen en color (asume BGR)
-            im = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            ax.imshow(im)
-
-        if titles[i] is not None:
-            ax.set_title(titles[i])
+    for i, ax in enumerate(axes):
+        if i < len(images):
+            display_image(ax, images[i], titles[i])
+        else:
+            ax.axis('off')
 
     if title:
         fig.text(0.5, 1, title, ha='center', fontsize=12)
@@ -74,7 +67,4 @@ def show_images(
     if show:
         plt.show()
         return None
-
-    else:
-
-        return fig, axes
+    return fig, axes
